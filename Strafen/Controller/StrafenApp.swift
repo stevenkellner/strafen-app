bb08d2bf71c37ea3b3442fb5a08aaac0170dc037
Steven Kellner <steven.kellner@web.de>
2020-07-24 09:30:51 +0200

    ShortcutItem only if app is fully closed

--- Strafen/Controller/StrafenApp.swift
+++ Strafen/Controller/StrafenApp.swift
@@ -6,12 +6,35 @@
 //
 
 import SwiftUI
-
 @main
 struct StrafenApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    @Environment(\.scenePhase) private var scenePhase
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }.onChange(of: scenePhase) { phase in
+            if phase == .background {
+                ImageData.shared.personImage = []
+            }
         }
+    }
+}
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        if let shortcutItem = options.shortcutItem {
+            switch shortcutItem.type {
+            case "profileDetail":
+                HomeTabs.shared.active = .profileDetail
+            case "personList":
+                HomeTabs.shared.active = .personList
+            case "reasonList":
+                HomeTabs.shared.active = .reasonList
+            default:
+                break
+            }
+        }
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 }
