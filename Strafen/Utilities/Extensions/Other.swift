029847379da27aaf6ede9e0d9d82244cdcbb767c
Steven Kellner <steven.kellner@web.de>
2020-08-26 12:08:23 +0200

    Added UINavigationController extension to swipe back  in NavigationViews

--- Strafen/Utilities/Extensions/Other.swift
+++ Strafen/Utilities/Extensions/Other.swift
@@ -157,3 +157,18 @@ extension UIApplication {
         sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
+
+// Extension of UINavigationController to swipe back in NavigationViews
+extension UINavigationController: UIGestureRecognizerDelegate {
+    
+    // Override viewDidLoad
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    // Override gestureRecognizerShouldBegin
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        viewControllers.count > 1
+    }
+}
