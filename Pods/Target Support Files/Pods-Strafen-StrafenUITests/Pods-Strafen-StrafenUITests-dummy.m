643419616b0101f92fe5eb0a493504ce9ec4a9bf
Steven Kellner <steven.kellner@web.de>
2021-01-03 10:50:09 +0100

    cleaned Pods and .gitignore

--- Pods/Target Support Files/Pods-Strafen-StrafenUITests/Pods-Strafen-StrafenUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Strafen_StrafenUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Strafen_StrafenUITests
-@end
--- Pods/Target Support Files/Pods-Strafen/Pods-Strafen-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Strafen : NSObject
-@end
-@implementation PodsDummy_Pods_Strafen
-@end
